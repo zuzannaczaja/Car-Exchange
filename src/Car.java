b708a7968a001ce66dd9c9d4511593bf489d28c0
zuzannaczaja <56079308+zuzannaczaja@users.noreply.github.com>
2020-10-13 11:15:51 +0200

    Added variables
    Added variables in class Car

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,70 @@
 public class Car {
 
+    String brand;
+    String model;
+    String bodyType;
+    String engineType;
+    int engineCapacity;
+    int yearOfProduction;
+    int price;
 
+    public Car(){
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public int getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public void setEngineCapacity(int engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
