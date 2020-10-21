9f9bc9dab89397af47b660a03fa31b429b4094c5
Michał Zembroń <59204659+michalzembron@users.noreply.github.com>
2020-10-21 15:12:41 +0200

    Wyklad

--- src/CarSellerAgent.java
+++ src/CarSellerAgent.java
@@ -5,14 +5,14 @@ public class CarSellerAgent extends Agent {
     // The catalogue of books for sale (maps the title of a book to its price)
     private Hashtable catalogue;
     // The GUI by means of which the user can add books in the catalogue
-    private BookSellerGui myGui;
+    //private BookSellerGui myGui;
     // Put agent initializations here
     protected void setup() {
         // Create the catalogue
         catalogue = new Hashtable();
         // Create and show the GUI
-        myGui = new BookSellerGui(this);
-        myGui.show();
+        //myGui = new BookSellerGui(this);
+        //myGui.show();
         // Add the behaviour serving requests for offer from buyer agents
         addBehaviour(new OfferRequestsServer());
         // Add the behaviour serving purchase orders from buyer agents
@@ -21,7 +21,7 @@ public class CarSellerAgent extends Agent {
     // Put agent clean-up operations here
     protected void takeDown() {
         // Close the GUI
-        myGui.dispose();
+        //myGui.dispose();
         // Printout a dismissal message
         System.out.println("Seller-agent "+getAID().getName()+" terminating.");
     }
---
+++ src/wyklad/GenethicAlgorithm.java
@@ -0,0 +1,12 @@
+package wyklad;
+
+import java.util.Random;
+
+public class GenethicAlgorithm {
+    public static void main(String[] args){
+        byte [] osobniki = {0b01101, 0b11000, 0b01000, 0b10011};
+        Random rand = new Random();
+
+        System.out.println("test");
+    }
+}
