daf0e118066ecf236f3f6cb856baa73899becce7
Michał Zembroń <59204659+michalzembron@users.noreply.github.com>
2020-10-25 20:07:16 +0100

    Agent Update
    * CarBuyerAgent
    * CarSellerAgent
    * CarSellerGui
    * Moved OfferRequestsServer to CarSellerAgent as an inner class
    * Moved RequestPerformer to CarBuyerAgent as an inner class

--- src/CarSellerAgent.java
+++ src/CarSellerAgent.java
@@ -1,30 +1,65 @@
 import jade.core.Agent;
 import jade.core.behaviours.*;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.MessageTemplate;
+
 import java.util.*;
 public class CarSellerAgent extends Agent {
     // The catalogue of books for sale (maps the title of a book to its price)
     private Hashtable catalogue;
     // The GUI by means of which the user can add books in the catalogue
-    //private BookSellerGui myGui;
+    private CarSellerGui myGui;
+
     // Put agent initializations here
     protected void setup() {
         // Create the catalogue
         catalogue = new Hashtable();
+
         // Create and show the GUI
-        //myGui = new BookSellerGui(this);
-        //myGui.show();
-        // Add the behaviour serving requests for offer from buyer agents
+        myGui = new CarSellerGui(this);
+        myGui.showGui();
+
+        // Register the book-selling service in the yellow pages
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("book-selling");
+        sd.setName("JADE-book-trading");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+
+        // Add the behaviour serving queries from buyer agents
         addBehaviour(new OfferRequestsServer());
+
         // Add the behaviour serving purchase orders from buyer agents
         addBehaviour(new PurchaseOrdersServer());
     }
+
     // Put agent clean-up operations here
     protected void takeDown() {
+        // Deregister from the yellow pages
+        try {
+            DFService.deregister(this);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
         // Close the GUI
-        //myGui.dispose();
+        myGui.dispose();
         // Printout a dismissal message
         System.out.println("Seller-agent "+getAID().getName()+" terminating.");
     }
+
     /**
      This is invoked by the GUI when the user adds a new book for sale
      */
@@ -32,7 +67,44 @@ public class CarSellerAgent extends Agent {
         addBehaviour(new OneShotBehaviour() {
             public void action() {
                 catalogue.put(title, new Integer(price));
+                System.out.println(title+" inserted into catalogue. Price = "+price);
             }
         } );
     }
+
+    /**
+     Inner class OfferRequestsServer.
+     This is the behaviour used by Book-seller agents to serve incoming requests
+     for offer from buyer agents.
+     If the requested book is in the local catalogue the seller agent replies
+     with a PROPOSE message specifying the price. Otherwise a REFUSE message is
+     sent back.
+     */
+    private class OfferRequestsServer extends CyclicBehaviour {
+        public void action() {
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                // Message received. Process it
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+                Integer price = (Integer) catalogue.get(title);
+                if (price != null) {
+                    // The requested book is available for sale. Reply with the price
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(price.intValue()));
+                }
+                else {
+                    // The requested book is NOT available for sale.
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            }
+            else {
+                block();
+            }
+        }
+    }
+    // End of inner class OfferRequestsServer
 }
